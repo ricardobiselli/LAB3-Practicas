c6d6718e2a6726b44adaac318714c5c1e5d30d31
ricardobiselli <ricardobiselli@gmail.com>
2024-04-04 22:33:42 -0300

    first commit

---
+++ lab3-practica1/src/app.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'preact/hooks'
+//import preactLogo from './assets/preact.svg'
+//import viteLogo from '/vite.svg'
+import './app.css'
+
+import React from 'react';
+import Movies from './assets/components/movies/movies';
+
+function App() {
+  return (
+    <div className="App">
+      <Movies />
+    </div>
+  );
+}
+
+export default App;
