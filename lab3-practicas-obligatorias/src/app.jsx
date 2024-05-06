d2fcd1e372fb2e99865e66c71781ff591706f5cc
ricardobiselli <ricardobiselli@gmail.com>
2024-05-06 14:30:09 -0300

    practica nro 3

--- lab3-practicas-obligatorias/src/app.jsx
+++ lab3-practicas-obligatorias/src/app.jsx
@@ -1,26 +1,14 @@
-  import React from 'react';
-  import './app.css'
-  import Table from './assets/components/table/Table';
+import React from 'react';
+import './app.css';
+import Login from './assets/components/login/login';
 
-  const App = () => {
-    const netIncomes = [
-      { brand: 'McDonalds', income: 1291283 },
-      { brand: 'Burger King', income: 1927361 },
-      { brand: 'KFC', income: 1098463 }
-    ];
+function App() {
+  return (
+    <div>
+      <p>LAB 3 - práctica obligatoria 3</p>
+      <Login />
+    </div>
+  );
+}
 
-    const totalIncome = netIncomes.reduce((acc, curr) => acc + curr.income, 0);
-    const averageIncome = totalIncome / netIncomes.length;
-
-    return (
-      <div>
-        <h1>LAB3 práctica 2</h1>
-        <h2>Net income table:</h2>
-
-        <Table data={netIncomes} />
-        <p>Average net income: {averageIncome.toFixed(1)}</p>
-      </div>
-    );
-  };
-
-  export default App;
+export default App;
